ff78f3fa815c10b60f3391dca0c4ccb7d6b5e738
jose-gabriel-bm <josegabrielbm@outlook.com>
2021-09-24 15:28:47 -0300

    correção no arquivo JS e adicionado icones

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.sidebar-toggle').on ('click',function(){
+    //Apresentar ou ocultar o menu
+    $('.sidebar-toggle').on('click', function () {
         $('.sidebar').toggleClass('toggled');
     });
-});
